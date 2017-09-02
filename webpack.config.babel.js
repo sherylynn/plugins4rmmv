3f033fbbb5ed28f907247b519635fc3c08b9b6a2
sherylynn <352281674@qq.com>
2017-09-03 00:44:59 +0800

    base

---
+++ webpack.config.babel.js
@@ -0,0 +1,42 @@
+import webpack from 'webpack';
+import fs from 'fs';
+import path from 'path';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+export default {
+    cache: true,
+    entry: [
+        './src/index.js'
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+      contentBase: './',
+      hot: true
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'js/plugins/')
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: [
+                path.resolve(__dirname, "node_modules"),
+            ],
+        }, {
+            test: /\.json$/,
+            loader: 'json-loader'
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Hot Module Replacement'
+          }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+    ],
+    resolve: {
+        extensions: ['*', '.js', '.json']
+    }
+}
