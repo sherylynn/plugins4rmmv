588a193a8a12dd87610f8b0fff215e89d918b80b
sherylynn <352281674@qq.com>
2017-09-14 00:57:41 +0800

    update release

---
+++ webpack.config.build.js
@@ -0,0 +1,49 @@
+const webpack= require('webpack')
+const path =require('path')
+const packageInfo=require('./package.json')
+let release='app.'+packageInfo.version+'.js'
+module.exports={
+  //cache : true,
+  entry : {
+    app:'./src/index.js'
+  },
+  devtool : 'inline-source-map',
+  devServer : {
+    contentBase: path.join(__dirname, 'game'),
+    hot: true,
+    publicPath:'/js/plugins/',
+    //需要设置一下位置
+    watchContentBase: true,
+    open:true
+  },
+  module : {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: [path.resolve(__dirname, "node_modules")]
+      }, {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }, {
+        test: /\.html$/,
+        loader: 'html-loader'
+      }
+    ]
+  },
+  plugins : [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ],
+  output : {
+    filename: release,
+    path: path.resolve(__dirname, 'game/js/plugins/'),
+    publicPath:'/js/plugins/'
+  },
+  resolve : {
+    extensions: ['*', '.js', '.json']
+  }
+}
